a3876c52f5da588c5966d02e808125390113446f
pedrohenrique <160751245+PedroHenriques1@users.noreply.github.com>
2025-05-23 16:16:50 -0300

    Java Controller
    Java Controller
    
    Co-Authored-By: DevPedrotestes <213092042+DevPedrotestes@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,12 +1,9 @@
 // comentário isso aqui é só um teste para o git //
 console.log("Olá Mundo");
 console.log("Olá Mundo");
-console.log("Olá Mundo");
+
     
 
 
 
-console.log("Olá Mundo");
-console.log("Olá Mundo");
-console.log("Olá Mundo");
 
