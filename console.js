c5c32492c47e0874ee36d2214de4baca71076760
pedrohenrique <160751245+PedroHenriques1@users.noreply.github.com>
2025-05-23 16:34:12 -0300

    teste
    teste
    
    Co-Authored-By: DevPedrotestes <213092042+DevPedrotestes@users.noreply.github.com>

--- console.js
+++ console.js
@@ -1,7 +1,7 @@
 // comentário isso aqui é só um teste para o git //
 // testando //
 console.log("Olá Mundo")
-console.log("Olá Mundo")
+console.log("Olá Mundo");
 console.log("Olá Mundo");
 console.log("Olá Mundo");
 console.log("Java Model Controller");
